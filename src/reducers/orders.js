a575cc7e4b9c54d9f827f7d672557260191d1260
Eduardo <jeduardorjx@gmail.com>
2019-11-06 11:15:28 -0700

    Add a new case to return state plus a new order

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -2,6 +2,9 @@ export const orders = (state = [], action) => {
   switch (action.type) {
     case 'SET_ORDERS':
       return action.orders;
+    case 'NEW_ORDER':
+      console.log(state)
+      return [...state, action.newOrder]
     default:
       return state;
   }
