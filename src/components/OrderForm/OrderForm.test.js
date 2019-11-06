f9fe6326f522eba5ce1a0d0aba1336bf64b7a565
Eduardo <jeduardorjx@gmail.com>
2019-11-06 11:47:46 -0700

    Setup test structure

--- src/components/OrderForm/OrderForm.test.js
+++ src/components/OrderForm/OrderForm.test.js
@@ -1,3 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { OrderForm, mapStateToProps, mapDispatchToProps } from './OrderForm'
+import { OrderForm, mapStateToProps, mapDispatchToProps } from './OrderForm';
+
+describe('OrderFromContainer', () => {
+
+  describe('OrderFrom component', () => {
+
+  });
+
+  describe('mapStateToProps', () => {
+
+  });
+
+  describe('mapDispatchToProps', () => {
+    
+  });
+
+});
--- src/components/Orders/Orders.test.js
+++ src/components/Orders/Orders.test.js
@@ -1,3 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Orders, mapStateToProps, mapDispatchToProps } from './Orders'
+
+describe('OrdersContainer', () => {
+
+  describe('Orders component', () => {
+
+  });
+
+  describe('mapStateToProps', () => {
+
+  });
+
+  describe('mapDispatchToProps', () => {
+    
+  });
+
+});
