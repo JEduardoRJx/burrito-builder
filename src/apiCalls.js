8bb2478aea00f3c321067ee36abf1ba09252ca83
Eduardo <jeduardorjx@gmail.com>
2019-11-06 11:12:43 -0700

    Create a new POST api call to post a new order

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,3 +7,25 @@ export const getOrders = async () => {
   const { orders } = data
   return orders
 }
+
+export const postOrder = async order => {
+  const url ='http://localhost:3001/api/v1/orders';
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({
+      id: order.id,
+      name: order.name,
+      ingredients: order.ingredients
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  const response = await fetch(url, options);
+  if(!response.ok) {
+    throw Error('Unable to create order. Try again later.')
+  }
+  console.log(response)
+  const newOrder = response.json();
+  return newOrder;
+}
