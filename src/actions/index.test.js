f13fd08f57f436d9814b2750d0976e7cdc8efd94
Eduardo <jeduardorjx@gmail.com>
2019-11-06 11:29:01 -0700

    Set two tests for actions

---
+++ src/actions/index.test.js
@@ -0,0 +1,37 @@
+import * as actions from './index';
+
+describe('actions', () => {
+
+  it('should have type of SET_ORDERS', () => {
+    const orders = [{
+      id: 1,
+      ingredients: (5) ["beans", "lettuce", "carnitas", "queso fresco", "jalapeno"],
+      name: "Pat"
+    }, {
+      id: 2,
+      ingredients: (5) ["beans", "rice", "carnitas", "jalapeno"],
+      name: "SAM"
+    }]
+    const expectedAction = {
+      type: 'SET_ORDERS',
+      orders
+    }
+    const result = actions.setOrders(orders)
+    expect(result).toEqual(expectedAction)
+  });
+
+  it('should have type of NEW_ORDER', () => {
+    const newOrder = [{
+      id: 1,
+      ingredients: (5) ["beans", "lettuce", "carnitas", "queso fresco", "jalapeno"],
+      name: "Pat"
+    }]
+    const expectedAction = {
+      type: 'NEW_ORDER',
+      newOrder
+    }
+    const result = actions.setNewOrder(newOrder)
+    expect(result).toEqual(expectedAction)
+  });
+
+})
