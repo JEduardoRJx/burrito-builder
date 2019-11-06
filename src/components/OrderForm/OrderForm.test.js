cb8891bc0684d5ec8328fca171c501b10212b0b9
Eduardo <jeduardorjx@gmail.com>
2019-11-06 11:54:53 -0700

    Test mapStateToProps and mapDispatchToProps

--- src/components/OrderForm/OrderForm.test.js
+++ src/components/OrderForm/OrderForm.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { OrderForm, mapStateToProps, mapDispatchToProps } from './OrderForm';
+import { setNewOrder } from '../../actions'
 
 describe('OrderFromContainer', () => {
 
@@ -9,11 +10,41 @@ describe('OrderFromContainer', () => {
   });
 
   describe('mapStateToProps', () => {
-
+    it('should return orders(array)', () => {
+      const orders = [{
+        id: 1,
+        ingredients: ["beans", "lettuce", "carnitas", "queso fresco", "jalapeno"],
+        name: "Pat"
+      }, {
+        id: 2,
+        ingredients: ["beans", "rice", "carnitas", "jalapeno"],
+        name: "SAM"
+      }]
+      const mockState = {
+        orders
+      }
+      const expected = {
+        orders
+      }
+      const mappedProps = mapStateToProps(mockState);
+      expect(mappedProps).toEqual(expected);
+    });
   });
 
   describe('mapDispatchToProps', () => {
-    
+    it('calls dispatch with a newOrder when setNewOrder is called', () => {
+      const mockDispatch = jest.fn();
+      const newOrder = {
+        id: 1,
+        ingredients: ["beans", "lettuce", "carnitas", "queso fresco", "jalapeno"],
+        name: "Pat"
+      }
+      const actionToDispatch = setNewOrder(newOrder);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      
+      mappedProps.setNewOrder(newOrder);
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
   });
 
 });
--- src/components/Orders/Orders.test.js
+++ src/components/Orders/Orders.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Orders, mapStateToProps, mapDispatchToProps } from './Orders'
+import { setOrders } from '../../actions';
 
 describe('OrdersContainer', () => {
 
@@ -9,11 +10,44 @@ describe('OrdersContainer', () => {
   });
 
   describe('mapStateToProps', () => {
-
+    it('should return orders(array)', () => {
+      const orders = [{
+        id: 1,
+        ingredients: ["beans", "lettuce", "carnitas", "queso fresco", "jalapeno"],
+        name: "Pat"
+      }, {
+        id: 2,
+        ingredients: ["beans", "rice", "carnitas", "jalapeno"],
+        name: "SAM"
+      }]
+      const mockState = {
+        orders
+      }
+      const expected = {
+        orders
+      }
+      const mappedProps = mapStateToProps(mockState);
+      expect(mappedProps).toEqual(expected);
+    });
   });
 
   describe('mapDispatchToProps', () => {
-    
+    it('calls dispatch with a newOrder when setNewOrder is called', () => {
+      const mockDispatch = jest.fn();
+      const orders = [{
+        id: 1,
+        ingredients: ["beans", "lettuce", "carnitas", "queso fresco", "jalapeno"],
+        name: "Pat"
+      }, {
+        id: 2,
+        ingredients: ["beans", "rice", "carnitas", "jalapeno"],
+        name: "SAM"
+      }];
+      const actionToDispatch = setOrders(orders);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.setOrders(orders);
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
   });
 
 });
